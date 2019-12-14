b73d43f346b44f688ffeb50af06331088096de19
Daewon Kwon <sliwet@gmail.com>
2019-12-13 20:42:52 -0500

    Complete

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,27 +1,132 @@
-const fs = require('fs');
 
-let myMap = L.map("map", {
-    center: [25,-25],
-    zoom: 2
+let magToRadius = mag => mag * 20000;
+
+let magToColor = magnitude => {
+    let a = 1.0;
+    if (magnitude < 1) {
+        return `rgba(0,255,0,${a})`
+    } else if (magnitude < 2) {
+        return `rgba(51,204,0,${a})`
+    } else if (magnitude < 3) {
+        return `rgba(102,153,0,${a})`
+    } else if (magnitude < 4) {
+        return `rgba(153,102,0,${a})`
+    } else if (magnitude < 5) {
+        return `rgba(204,51,0,${a})`
+    } else {
+        return `rgba(255,0,0,${a})`
+    }
+}
+
+// let link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+let link = "static/data/earthquake.geojson";
+let linktectonicplates = "static/data/PB2002_plates.json";
+
+d3.json(link, data => {
+    d3.json(linktectonicplates,tpdata =>{
+        let layerList = {
+            earthquakes: getEarthquakes(data),
+            faultlines: getFaultlines(tpdata)
+        };
+        createMap(layerList);
+    });
 });
 
-L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    maxZoom: 18,
-    id: "mapbox.streets",
-    accessToken: API_KEY
-}).addTo(myMap);
+let getFaultlines = tpdata => {
+    let plateLines = L.geoJSON(tpdata, {
+        color: "yellow", 
+        fillOpacity: 0
+    })
+    return plateLines;
+}
 
+let getEarthquakes = earthquakeData => {
+    let cmarkers = [];
+    earthquakeData.features.forEach(feature => {
+        let crd = [feature.geometry.coordinates[1], feature.geometry.coordinates[0]];
+        let onemarker = L.circle(crd, {
+            stroke: false,
+            color: magToColor(feature.properties.mag),
+            fillColor: magToColor(feature.properties.mag),
+            fillOpacity: 1,
+            radius: magToRadius(feature.properties.mag)
+        });
 
+        onemarker.bindPopup(
+            "<h3>" + feature.properties.place + "<br>Magnitude: " + feature.properties.mag
+            + "<br>Time: " + new Date(feature.properties.time)
+        );
+        cmarkers.push(onemarker);
+    });
 
-let link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+    return L.layerGroup(cmarkers);
+}
 
-d3.json(link, geojsondata => {
+let createMap = layerList => {
+    let earthquakes = layerList.earthquakes;
+    let faultlines = layerList.faultlines;
+    let maxzoom = 18;
+    
+    let satellitemap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        maxZoom: maxzoom,
+        id: "mapbox.satellite",
+        accessToken: API_KEY
+    });
 
-    fs.writeFile('Output.txt', geojsondata, (err) => { 
-      
-        // In case of a error throw err. 
-        if (err) throw err; 
-    }) 
-    // console.log(geojsondata);
-}); 
+    let grayscalemap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        maxZoom: maxzoom,
+        id: "mapbox.light",
+        accessToken: API_KEY
+    });
+
+    let outdoormap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        maxZoom: maxzoom,
+        id: "mapbox.outdoors",
+        accessToken: API_KEY
+    });
+
+    let baseMaps = {
+        "Satellite": satellitemap,
+        "Grayscale": grayscalemap,
+        "Outdoors":outdoormap
+    };
+
+    let overlayMaps = {
+        "Fault Lines": faultlines,
+        Earthquakes: earthquakes
+    };
+
+    let myMap = L.map("map", {
+        center: [40, -100],
+        zoom: 5,
+        layers: [satellitemap,faultlines, earthquakes]
+    });
+
+    L.control.layers(baseMaps, overlayMaps, {
+        collapsed: false
+    }).addTo(myMap);
+
+    let info = L.control({
+        position: "bottomright"
+    });
+
+    info.onAdd = () => {
+        let infodiv = L.DomUtil.create("div", "legend");
+        let mags = [0, 1, 2, 3, 4, 5];
+
+        mags.forEach(mag => {
+            let magRange = `${mag}-${mag+1}`;
+            if (mag >= 5) { magRange = `${mag}+`}
+            let html = `<div class="legend-item">
+                    <div style="height: 25px; width: 25px; background-color:${magToColor(mag)}"> </div>
+                    <div class=legend-text>${magRange}</div>
+                </div>`
+            infodiv.innerHTML += html
+        });
+        return infodiv;
+    };
+    info.addTo(myMap);
+}
